30f0e66148dfa559919f4ec95f74a5e31605d06c
Brendan Dagys <brendandagys@gmail.com>
2021-06-23 20:48:34 -0400

    Almost finished reset Submission implementation

--- frontend/src/screens/FormScreen.tsx
+++ frontend/src/screens/FormScreen.tsx
@@ -9,7 +9,7 @@ import Modal from 'react-bootstrap/Modal'
 import { useTypedSelector } from '../hooks/useTypedSelector'
 
 const FormScreen = (): JSX.Element => {
-  const [show, setShow] = useState(true)
+  const [show, setShow] = useState(false)
   const toggleShow = () => setShow((show) => !show)
 
   const [showFail, setShowFail] = useState(false)
@@ -19,20 +19,39 @@ const FormScreen = (): JSX.Element => {
   const [numQuestionsAnswered, setNumQuestionsAnswered] = useState(0)
 
   const { userInfo } = useTypedSelector((state) => state.authenticate)
-
+  const { submission } = useTypedSelector((state) => state.submissionFetch)
   const { loading, error, questions } = useTypedSelector(
     (state) => state.questionsFetch
   )
 
-  const { getQuestions } = useActions()
+  const submitAnswers = questions?.map((question) => {
+    return { question: question._id, answer: 'no' }
+  })
+
+  const { getQuestions, fetchSubmission, createSubmission } = useActions()
 
   useEffect(() => {
-    getQuestions()
-  }, [getQuestions])
+    const timer = setTimeout(() => {
+      setShow(true)
+    }, 700)
+    return () => clearTimeout(timer)
+  }, [])
+
+  useEffect(() => {
+    if (!questions) getQuestions()
+  }, [questions, getQuestions])
+
+  const [userResetFlag, setUserResetFlag] = useState(false)
+
+  useEffect(() => {
+    if (!userResetFlag) fetchSubmission()
+  }, [userResetFlag, fetchSubmission])
 
   useEffect(() => {
     if (numQuestionsAnswered === questions?.length) {
       setAllQuestionsAnswered(true)
+    } else {
+      setAllQuestionsAnswered(false)
     }
   }, [numQuestionsAnswered, questions])
 
@@ -44,12 +63,12 @@ const FormScreen = (): JSX.Element => {
 
   return (
     <>
-      <Container>
+      <Container className='pb-5 text-center' style={{ maxWidth: '600px' }}>
         <h2 className='mb-3'>Screening Questions</h2>
         {questions?.map((question, index) => {
           return (
             <Question
-              key={question.id}
+              key={index}
               questionText={question.question}
               index={index}
               markQuestionAnswered={markQuestionAnswered}
@@ -58,29 +77,38 @@ const FormScreen = (): JSX.Element => {
           )
         })}
         {allQuestionsAnswered && (
-          <Button style={{ width: '100%' }} type='submit' variant='primary'>
+          <Button
+            size='lg'
+            className='mt-3'
+            style={{ width: '100%', height: '10vh', fontSize: '3rem' }}
+            type='submit'
+            variant='primary'
+            onClick={() => createSubmission(submitAnswers, false)}
+          >
             Submit
           </Button>
         )}
       </Container>
-      <Modal show={show} onHide={toggleShow}>
-        <Modal.Header style={{ backgroundColor: '#F0F0F0' }}>
+      <Modal show={show} onHide={toggleShow} className='text-center'>
+        <Modal.Body style={{ backgroundColor: '#F0F0F0' }}>
           <Modal.Title>
             <p className='text-success'>{'Pre-Screening Messages'}</p>
             <small className='text-muted'>{`${userInfo?.firstName} ${userInfo?.lastName}`}</small>
           </Modal.Title>
-        </Modal.Header>
+        </Modal.Body>
         <Modal.Body>
           <h6>
             Screening for staff for symptoms of COVID is important in order to
-            keep our patients and staff safe. In order to maximize the efficacy
-            of screening we would ask that you be honest about your symptoms and
-            complete the screening questions within 2 hours of your shift start
-            time.
+            keep our patients and staff safe.
+          </h6>
+          <h6>
+            In order to maximize the efficacy of screening we would ask that you
+            be honest about your symptoms and complete the screening questions
+            within 2 hours of your shift start time.
           </h6>
           <h6>
             If you recently failed screening and have been authorized to return
-            to work byOHS then you are OK to proceed as long as you have no new
+            to work by OHS then you are OK to proceed as long as you have no new
             symptoms.
           </h6>
         </Modal.Body>
@@ -94,13 +122,13 @@ const FormScreen = (): JSX.Element => {
           </Button>
         </Modal.Footer>
       </Modal>
-      <Modal show={showFail} onHide={toggleShowFail}>
-        <Modal.Header style={{ backgroundColor: '#F0F0F0' }}>
+      <Modal show={showFail} onHide={toggleShowFail} className='text-center'>
+        <Modal.Body style={{ backgroundColor: '#F0F0F0' }}>
           <Modal.Title>
             <p className='text-danger'>Contact Us</p>
             <small className='text-muted'>{`${userInfo?.firstName} ${userInfo?.lastName}`}</small>
           </Modal.Title>
-        </Modal.Header>
+        </Modal.Body>
         <Modal.Body>
           <h6 className='text-danger'>
             If you are experiencing any of the symptoms listed below, please see
